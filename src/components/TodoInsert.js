522cdd2220dd0f0cf054450f84514e56d84c0384
PhilosopherProgrammer <nekarakube@gmail.com>
2022-05-14 22:56:51 +0900

    add insert

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback((e) => {
+        setValue(e.target.value);
+    }, []);
+
+    const onClick = useCallback((e) => {
+        e.preventDefault();
+        onInsert(value);
+        setValue('');
+    }, [onInsert, value]);
+
     return (
         <form className="TodoInsert">
-            <input placeholder='할 일을 입력하세요' />
-            <button type='submit'>
+            <input placeholder='할 일을 입력하세요' value={ value } onChange={onChange} />
+            <button type='submit' onClick={onClick}>
                 <MdAdd />
             </button>
         </form>
